66e6c5b7697fced60b21cd85c0271ca91cda91c2
Polvere243 <danielercoli@gmail.com>
2024-01-29 20:51:12 +0100

    printed the list of emails in page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 console.log("Vue Ok", Vue);
 
+// Indirizzo per fare la chiamata
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
+
 const {createApp} = Vue;
 
 const app = createApp (
     {
-        data: () => {
-
+        data: () => ({
+            addresses: []
+            
+        }),
+        methods:{
+            getRandomAddress() {
+                for (let i = 0; i < 10; i++) {
+                    axios.get(endpoint).then((res) => {
+                        this.addresses.push(res.data.response);
+                        console.log(res.data.response);
+                    })
+                }
+            }
+        },
+        created() {
+            this.getRandomAddress();
         }
-    }
+    },
 )
+
 
 app.mount("#root");
